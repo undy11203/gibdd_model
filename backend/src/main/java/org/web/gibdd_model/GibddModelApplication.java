69fb56b2f3bedc43b1cfeef2f5f46ade8f0c6420
undy11203 <shabanov.dima11203@gmail.com>
2025-03-19 22:32:40 +0700

    first commit

---
+++ backend/src/main/java/org/web/gibdd_model/GibddModelApplication.java
@@ -0,0 +1,13 @@
+package org.web.gibdd_model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GibddModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GibddModelApplication.class, args);
+    }
+
+}
